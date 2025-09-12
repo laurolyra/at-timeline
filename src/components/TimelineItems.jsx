7da70c9a92454d290e86769bf836bef5bd2b96ea
Lauro Lyra <laurolyra@gmail.com>
2025-09-12 19:14:05 -0300

    allUniqueDates; timeline in construction

--- src/components/TimelineItems.jsx
+++ src/components/TimelineItems.jsx
@@ -3,10 +3,29 @@ import assignLanes from "../services/assignLanes.js";
 const TimelineItems = ({ items }) => {
   const lanes = assignLanes(items);
   console.log('lanes', lanes);
+  const allUniqueDates = [...new Set(lanes.flatMap((lane) => lane.map((item) => item.start)))];
+  console.log('allUniqueDates', allUniqueDates);
   return (
     <div>
-      {items.map((lane) => (
-        <div key={lane.id}>{lane.name}</div>
+        <table>
+            <thead>
+                <tr>{allUniqueDates.map((date) => (
+                    <th key={date}>{date}</th>
+                ))}</tr>
+            </thead>
+            <tbody>
+                {lanes.map((lane) => (
+                    <tr key={lane.id}>
+                        {allUniqueDates.map((date) => (
+                            <td key={date}>{lane.find((item) => item.start === date)?.name || ''}</td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        {items.map((lane) => (
+        <div key={lane.id}>{lane.name}  - {lane.start} - {lane.end}</div>
+
       ))}
     </div>
   );
