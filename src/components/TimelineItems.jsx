e2eacc7541b718b4cd3be0603f46db89e54999de
Lauro Lyra <laurolyra@gmail.com>
2025-09-12 20:01:18 -0300

    styling dates 'header'

--- src/components/TimelineItems.jsx
+++ src/components/TimelineItems.jsx
@@ -2,21 +2,28 @@ import assignLanes from "../services/assignLanes.js";
 import TimelineItem from "./TimelineItem.jsx";
 
 const TimelineItems = ({ items }) => {
-  const lanes = assignLanes(items);
-  console.log('lanes', lanes);
-  const allUniqueStartDates = [...new Set(lanes.flatMap((lane) => lane.map((item) => item.start)))];
-  const allUniqueEndDates = [...new Set(lanes.flatMap((lane) => lane.map((item) => item.end)))];
-  console.log('allUniqueStartDates', allUniqueStartDates);
-  console.log('allUniqueEndDates', allUniqueEndDates);
-  const allUniqueDates = [new Set([...allUniqueStartDates, ...allUniqueEndDates])];
-  console.log('allUniqueDates', allUniqueDates);
-  return (
+    const lanes = assignLanes(items);
+    console.log('lanes', lanes);
+    const allUniqueStartDates = [...new Set(lanes.flatMap((lane) => lane.map((item) => item.start)))];
+    const allUniqueEndDates = [...new Set(lanes.flatMap((lane) => lane.map((item) => item.end)))];
+    console.log('allUniqueStartDates', allUniqueStartDates);
+    console.log('allUniqueEndDates', allUniqueEndDates);
+    const allUniqueDates = [...new Set([...allUniqueStartDates, ...allUniqueEndDates])].sort((a, b) => new Date(a) - new Date(b))
+    console.log('allUniqueDates', allUniqueDates);
+    return (
     <div>
-        {allUniqueDates.map((date) => (
-            <div key={date}>{date}</div>
-        ))}
-        <TimelineItem start="2021-01-14" end="2021-01-22" name="Recruit translators" />
-       
+        <div className="date-label">
+            {allUniqueDates.map((date) => (
+                <p key={date}>{date}</p>
+            ))}
+        </div>
+        {
+            lanes[0].map((item) => (
+                <TimelineItem key={item.id} start={item.start} end={item.end} name={item.name} />
+            ))
+        }
+        {/* <TimelineItem start="2021-01-14" end="2021-01-22" name="Recruit translators" /> */}
+        
         {/* <table>
             <thead>
                 <tr>{allUniqueStartDates.map((date) => (
@@ -34,7 +41,7 @@ const TimelineItems = ({ items }) => {
             </tbody>
         </table> */}
     </div>
-  );
+    );
 };
 
 export default TimelineItems;
